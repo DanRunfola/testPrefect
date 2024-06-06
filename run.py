24f0066b9de862773c593aca165717558ddf2d44
DanRunfola <dan@danrunfola.com>
2024-06-06 09:46:50 -0400

    no cron

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ testFlow = flow.from_source(
 
 testFlow.deploy(name="testDeploy", 
                 work_pool_name="k8s-pool", 
-                parameters=dict(testParameter="Test Parameter Value"),
-                cron="*/1 * * * *")
+                parameters=dict(testParameter="Test Parameter Value"))
+                #,cron="*/1 * * * *")
